9ce3d16df4552aa7721dcf1001f1b6adc00eda7e
abdallah <abddoullah.bouzhar@gmail.com>
2022-12-17 13:14:03 +0100

    break up url

--- app/core/app.php
+++ app/core/app.php
@@ -9,13 +9,13 @@ class App
 
     public function __construct()
     {
-        $this->parseUrl();
+        $url = $this->parseUrl();
     }
 
     public function parseUrl()
     {
         if (isset($_GET['url'])) {
-            echo $_GET['url'];
+         return explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
         }
     }
 }
