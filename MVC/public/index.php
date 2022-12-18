c3ea44a748e78eb6b83930fdb0165c684e6927fa
abdallah <abddoullah.bouzhar@gmail.com>
2022-12-18 21:49:40 +0100

    router setting up

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,1 +1,33 @@
-hello world
+<?php
+session_start();
+function show($stuff)
+{
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+function splitURL()
+{
+    $URL = $_GET['url'] ?? 'home';
+    $URL = explode("/", $URL);
+    return $URL;
+}
+
+function loadController()
+{
+    $URL = splitURL();
+
+    $filename = '../app/controllers/'.ucfirst($URL[0]).'.php';
+    if(file_exists($filename))
+    {
+        require_once $filename;
+    }else 
+    {
+        $filename = '../app/controllers/_404.php';
+        require_once $filename;
+    }
+}
+
+loadController();
+//show(splitURL());
