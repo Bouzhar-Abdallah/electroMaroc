ee58fa34297f145c5bcbb3e16403da1832e241b5
abdallah <abddoullah.bouzhar@gmail.com>
2022-12-19 16:33:14 +0100

    navbar

--- MVC/app/views/home.view.php
+++ MVC/app/views/home.view.php
@@ -1,13 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=ROOT?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= ROOT ?>assets/css/style.css">
     <title>home</title>
 </head>
+
 <body>
-    <h1 class="my-60">home view</h1>
+
+    <header class="flex justify-between bg-cadet text-white">
+        <!-- <img src="" alt="" srcset=""> -->
+        <a href="#" class="block px-navbar-item py-navbar-item">logo</a>
+        <nav>
+            <ul class="grid grid-flow-col">
+                <li><a href="" class="block px-navbar-item py-navbar-item hover:bg-cadeth">acceuil</a></li>
+                <li><a href="" class="block px-navbar-item py-navbar-item hover:bg-cadeth">produits</a></li>
+                <li><a href="" class="block px-navbar-item py-navbar-item hover:bg-cadeth">contact</a></li>
+            </ul>
+        </nav>
+        <nav>
+            <ul class="grid grid-flow-col items-center">
+                <li><a href="" class="flex items-center px-navbar-item py-navbar-item hover:bg-cadeth">
+                        <svg class="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        </svg>
+                        cart
+                    </a></li>
+                <li>
+                    <a href="" class="flex items-center  px-navbar-item py-navbar-item hover:bg-cadeth">
+                        <svg class="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                        </svg>
+
+                        my account
+                    </a>
+                    <ul class="hidden">
+                        <li><a href="">profile</a></li>
+                        <li><a href="">orders</a></li>
+                        <li><a href="">bla bla</a></li>
+                        <li><a href="">logout</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="" class="flex items-center px-navbar-item py-navbar-item hover:bg-cadeth">
+
+                        <svg class="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                        </svg>
+
+                        login
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="flex items-center px-navbar-item py-navbar-item bg-saffron text-cadet hover:bg-wsaffron transition-colors">
+
+                        <svg class="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                        </svg>
+
+                        inscrivez vous
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
 </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: ["MVC/app/views/**/*.{html,js,php}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'cadet': '#2B2D42',
+        'cadeth': '#404363',
+        'mauve': '#361B2B',
+        'saffron': '#FEEA00',
+        'wsaffron': '#FFF15C',
+        'ryb': '#FF1B1C',
+        'cultured': '#F7F7F9',
+        'mediumaqua': '#41E2BA',
+        'goldenrod': '#F3F9D2',
+        'middlegreen': '#558564',
+        },
+    },
   },
   plugins: [],
 }
