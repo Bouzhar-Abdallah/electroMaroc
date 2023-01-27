1df525d93ed54f6650c4ac9608b5bebc80dd56d7
abdallah <abddoullah.bouzhar@gmail.com>
2023-01-27 14:39:35 +0100

    product_view created

--- other/views.sql
+++ other/views.sql
@@ -17,3 +17,10 @@ p.`libelle`, p.`id_photo_principale`,
 u.nom, u.prenom, u.adresse, u.ville, u.numero_tel
 from ligne_commande as l inner JOIN commande as c INNER JOIN produit as p INNER JOIN user as u 
 where l.id_commande = c.id && l.id_produit = p.id && u.id = c.id_client;
+
+create VIEW product_view AS
+SELECT 
+pro.id AS product_id, pro.`libelle`, pro.description, pro.prix_final,
+ph.photo
+from photo as ph INNER JOIN produit as pro INNER JOIN categorie as cat
+where pro.id = ph.id_produit && ph.display_order = 1 && pro.visibilite = 1 && cat.visibilite = 1 && pro.id_categorie = cat.id;
