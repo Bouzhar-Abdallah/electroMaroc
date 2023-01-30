536c0352d3e798ce9464808ea7b459ce54b5c0a1
abdallah <abddoullah.bouzhar@gmail.com>
2023-01-30 23:02:57 +0100

    all categories by default

--- MVC/app/controllers/produits.php
+++ MVC/app/controllers/produits.php
@@ -18,7 +18,7 @@ class Produits extends Controller
         //showd($produit->status);
         $this->view('produits',$data,'products-container');
     }
-    public function getproductsByCategory($id_category ='*', $limit = 10, $offset = '0'){
+    public function getproductsByCategory($id_category ='all', $limit = 10, $offset = '0'){
        
         $data = [];
         $produit = new Produit('products_view_user');
@@ -26,9 +26,13 @@ class Produits extends Controller
         $data['categories'] = $categorie->where(array('visibilite'=>1));
         $produit->setLimit($limit);
         $produit->setOffset($offset);
-        $data['produits'] = $produit->where(
-            array('id_categorie'=>$id_category)
-        );
+        if ($id_category == 'all') {
+            $data['produits'] = $produit->findAll();
+        }else{
+            $data['produits'] = $produit->where(
+                array('id_categorie'=>$id_category)
+            );
+        }
         //showd($produit->status);
         $this->view('produits_ajax',$data,'products-container_ajax');
     }
--- MVC/public/assets/js/categoriesAjax.js
+++ MVC/public/assets/js/categoriesAjax.js
@@ -1,7 +1,9 @@
 const products_container = document.querySelector('#products_container')
 
 class products{
-    constructor(_limit=10,_offset=0,_categorie){
-
+    constructor(_limit=10,_offset=0,_categorie='all'){
+        this.limit=_limit
+        this.offset=_offset
+        this.categorie=_categorie
     }
 }
