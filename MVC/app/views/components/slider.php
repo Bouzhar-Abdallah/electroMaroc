13c60248ff2d6c3ef26e322a9d7999dca18c478f
abdallah <abddoullah.bouzhar@gmail.com>
2023-01-31 23:12:09 +0100

    ready

--- MVC/app/views/components/slider.php
+++ MVC/app/views/components/slider.php
@@ -1,11 +1,10 @@
-<div class="bg-[#f3f8fe] pb-8 my-8">
+<div class="bg-cadethhh hover:bg-cadethh transition-all duration-700 ease-in pb-8 my-8">
   <div class="grid place-items-center my-8 pt-10">
-    <h1 class=" text-3xl font-bold">New Arrivals</h1>
-    <h1 class="text-center text-lg font-light px-4">Our Cold Weather Staples Will Help Keep Skin Hydrated & Happy All
-      Winter Long</h1>
+    <h1 class="text-cadet text-3xl font-bold">Proffitez de nos superbes promotions !</h1>
+    <h1 class="text-gray-600 text-center text-lg font-light px-4 mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa sapiente incidunt natus similique facere veniam nesciunt, mollitia non quasi saepe necessitatibus. Nemo sit itaque nobis id dolorum et veniam ducimus!</h1>
   </div>
   <section class=" flex items-center justify-around lg:mx-12">
-    <button id="left" class=" mx-4 bg-white hover:bg-slate-100\ rounded-full w-8 h-8 grid place-items-center">
+    <button id="left" class=" mx-4 bg-cadet hover:bg-saffron transition-all duration-300 ease-out text-saffron hover:text-cadet w-8 h-8 grid place-items-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path class="w-5 h-5" stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
 
@@ -19,7 +18,7 @@
         <!-- slider card -->
         <?php foreach ($data as $value) { ?>
 
-          <a href="<?=ROOT?>product/<?=$value['id']?>" class="mx-0.5 relative bg-white">
+          <a href="<?= ROOT ?>product/<?= $value['id'] ?>" class="mx-0.5 relative bg-white">
             <div class="test relative">
               <?php foreach ($value['photo'] as $key => $photo) { ?>
 
@@ -30,17 +29,17 @@
                   <img class="seocnd_image absolute z-10 hidden" src="data:image/jpeg;base64,<?= base64_encode($photo['photo']) ?>" alt="" srcset="">
                 <?php }; ?>
 
-              <?php }?>
+              <?php } ?>
             </div>
-              <!-- <img class="absolute " src="./src/images/first.webp" alt="" srcset=""> -->
+            <!-- <img class="absolute " src="./src/images/first.webp" alt="" srcset=""> -->
 
             <div class="flex flex-col items-center capitalize my-2 gap-0.5 relative py-2">
               <div class="font-thin mb-2 text-sm text-center text-gray-600 flex justify-between items-center w-full px-10">
-                <h1 class="text-cadet font-extrabold text-2xl py-1 px-2">$ 
-                  <?=$value['prix_offre'];?>
+                <h1 class="text-cadet font-extrabold text-2xl py-1 px-2">$
+                  <?= $value['prix_offre']; ?>
                 </h1>
                 <h1 class="text-gray-400 font-bold line-through text-xl  py-1 px-3">
-                $  <?=$value['prix_final'];?>
+                  $ <?= $value['prix_final']; ?>
                 </h1>
               </div>
               <h1 class="font-medium text-lg text-center relative text-gray-900"><?= $value['libelle'] ?></h1>
@@ -56,7 +55,7 @@
     </div>
 
 
-    <button id="right" class=" mx-4 bg-white hover:bg-slate-100\ rounded-full w-8 h-8 grid place-items-center">
+    <button id="right" class="mx-4 bg-cadet hover:bg-saffron transition-all duration-300 ease-out text-saffron hover:text-cadet w-8 h-8 grid place-items-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path class="w-5 h-5" stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
       </svg>
